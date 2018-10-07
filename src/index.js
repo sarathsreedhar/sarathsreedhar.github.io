0a0e5b90e9df829bcecc84f6394bca001fff240b
sarathsreedhar <sarath.knd@gmail.com>
2018-10-07 13:28:00 +0530

    update in intial file with web pack structure

---
+++ src/index.js
@@ -0,0 +1,11 @@
+ import _ from 'lodash';
+function component() {
+    let element = document.createElement('div');
+  
+    // Lodash, currently included via a script, is required for this line to work
+    element.innerHTML = _.join(['Hello sarath', 'webpack'], ' ');
+  
+    return element;
+  }
+  
+  document.body.appendChild(component());
